92acab0973c039819d273982bc118414872d8a54
pravin <pravinpgr9@gmail.com>
2021-05-27 07:59:31 +0530

    Testing

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -7,7 +7,19 @@ class News_model extends CI_Model {
     }
     
 
-    
+    public function get_news123($slug = FALSE)
+    {
+        if ($slug === FALSE)
+        {
+            $query = $this->db->get('news');
+            return $query->result_array();
+        }
+ 
+        $query = $this->db->get_where('news', array('slug' => $slug));
+        return $query->row_array();
+    }
+
+
     public function get_news($slug = FALSE)
     {
         if ($slug === FALSE)
